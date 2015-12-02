461cd2ff867f5dfeb45155fa544a7f87d72dca93
Miguel Concepcion <miguel.concepcion@outlook.com>
2015-12-02 16:17:17 +0800

    Changed the xpath, scrapes the title,price and sku.

--- allforyou_sg/allforyou_sg/items.py
+++ allforyou_sg/allforyou_sg/items.py
@@ -11,4 +11,6 @@ import scrapy
 class AllforyouSgItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    title = scrapy.Field()
+    price = scrapy.Field()
     pass
--- allforyou_sg/allforyou_sg/spiders/allforyou.py
+++ allforyou_sg/allforyou_sg/spiders/allforyou.py
@@ -1,19 +1,30 @@
 import scrapy
 
 
-from allforyou_sg.Items import AllforyouSgItem
-
-class AllForYouCrawler(Scrapy.Spider):
+from allforyou_sg.items import AllforyouSgItem
+class AllForYouCrawler(scrapy.Spider):
     name = "AFY"
     allowed_domains = ["www.allforyou.sg"]
     start_urls = [
-        "https://allforyou.sg/"]
+        "https://www.allforyou.sg/"]
 
     def parse(self, response):
-        for href in response.xpath('ul[@class="treemenu"//a/@href'):
+        for href in response.xpath('//a/@href'):
             url = response.urljoin(href.extract())
+           
             yield scrapy.Request(url, callback=self.parse_dir_contents)
             
     def parse_dir_contents(self,response):
+      
+               
+        for lnk in response.xpath('//div[@class="prod-data"]'):
+             items = AllforyouSgItem()
+             items['title'] = lnk.xpath('@data-name').extract()
+             items['price'] = lnk.xpath('@data-price').extract()
+             items['sku'] = lnk.xpath('@data-newprodid').extract()
 
-        pass
+             yield items
+  
+                    
+
+        
